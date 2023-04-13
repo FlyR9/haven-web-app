fb6dc1dc99bf71962238583dfe271d94c4654e13
FlyR9 <48998495+FlyR9@users.noreply.github.com>
2023-04-13 20:20:22 +0200

    http -> https

--- client/src/platforms/desktop/nodes.ts
+++ client/src/platforms/desktop/nodes.ts
@@ -1,24 +1,24 @@
 export const REMOTE_NODES = [
   {
-    address: "http://remote.haven.miner.rocks",
+    address: "https://remote.haven.miner.rocks",
     provider: "Haven Miner Rocks",
     port: "17750",
     trusted: true,
   },
   {
-    address: "http://nodes.hashvault.pro",
+    address: "https://nodes.hashvault.pro",
     provider: "Hash Vault",
     port: "17750",
     trusted: true,
   },
   {
-    address: "http://xhv-pst.minershive.ca",
+    address: "https://xhv-pst.minershive.ca",
     provider: "Miners Hive",
     port: "17750",
     trusted: true,
   },
    {
-     address: "http://remote.eu.havenprotocol.org",
+     address: "https://remote.eu.havenprotocol.org",
      provider: "Haven European Node",
      port: "17750",
      trusted: true,
